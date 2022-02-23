220139c75f79159f5451d2742e421fcddfbc936f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 15:41:34 +0900

    update1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,10 +10,10 @@ $(".menu").mouseover(menudown);
 $(".menu").mouseout(menuup);
 
 function menudown(){
-    $(".submenu").stop().slideDown();
+    $(".submenu").stop().slideDown(200);
 }
 function menuup(){
-    $(".submenu").stop().slideUp();
+    $(".submenu").stop().slideUp(200);
     
 }
 var a1;
@@ -54,6 +54,62 @@ $(".submenu li").mouseover(function(){
     $("#vertical_underline").offset({left:$(this).children("a").offset().left-10,top:$(this).offset().top+40})
     $("#vertical_underline").width(a2+"px")
 })
+//////////////////////헤더
+const copy_banner = $(".swiper-slide")
+$(copy_banner).clone().appendTo(".swiper-wrapper");
+$(copy_banner).clone().prependTo(".swiper-wrapper");
+const bullet = $(".swiper-pagination-bullet")
+$(bullet).clone().appendTo(".swiper-pagination");
+$(bullet).clone().prependTo(".swiper-pagination");
+$(".swiper-pagination-bullet:nth-of-type(2)").addClass("active");
+$(".swiper-container .arrow").click(function(){
+    if($(this).attr("class")=="arrow next"){
+        if(click==true){
+            click=false;
+            
+            let bleft = Number($(".swiper-wrapper").css("left").replace("px",""))
+            let bwidth = Number($(".swiper-slide").css("width").replace("px",""))
+            let index=Math.floor(-bleft/bwidth)%3;
+            $(".swiper-wrapper").css({"transition":"0.3s"});
+            $(".swiper-wrapper").css({"left":bleft+bwidth+"px"});
+            $(".swiper-pagination-bullet").eq(index).addClass("active");
+            $(".swiper-pagination-bullet").eq(index).siblings().removeClass("active");
+            setTimeout(function(){
+                if(index==1){
+                $(".swiper-wrapper").css({"transition":"0s"})
+                $(".swiper-wrapper").css({"left":(-3*bwidth)+"px"})
+            }
+                click=true;
+            },300)
+        }
+        
+        ///2일때 초기로
+    
+    }
+    if($(this).attr("class")=="arrow prev"){
+        if (click==true){
+            click=false;
+            
+            let bleft = Number($(".swiper-wrapper").css("left").replace("px",""))
+            let bwidth = Number($(".swiper-slide").css("width").replace("px",""))
+            let index= (Math.floor(-bleft/bwidth)-1)%3;
+            $(".swiper-wrapper").css({"transition":"0.3s"})
+            $(".swiper-wrapper").css({"left":bleft-bwidth+"px"})
+            $(".swiper-pagination-bullet").eq(index).addClass("active");
+            $(".swiper-pagination-bullet").eq(index).siblings().removeClass("active");
+            setTimeout(function(){
+                if(index==1){
+                $(".swiper-wrapper").css({"transition":"0s"})
+                $(".swiper-wrapper").css({"left":(-3*bwidth)+"px"})
+            }
+                click=true;
+            },300)
+        }
+    }
+})
+//////////////////////메인배너
+
+//////////////////////제품소개 슬라이드
 var product_info_value=0; ///신제품 자동선택
 var product_info_totalvalue = 3;///카테고리 갯수
 var info_index=1;
@@ -164,12 +220,11 @@ $(".product_info_index>li ul").draggable({
 				}
 			}
 		})
-/*function arrow(e){
+
+function arrow_notice(e){
             if(click==true){
                 click=false;
-                var a = $(".product_info_index li").width()/4
 				console.log("무브는"+move);
-				console.log(a);
                 index[move] += e;
 				console.log("인덱스는"+index);
                 $(".product_info_index li:nth-of-type("+(move+1)+") ul").css({"left":-1200 +( index[move] * -300) + "px","transition":"0.4s"});
@@ -187,7 +242,80 @@ $(".product_info_index>li ul").draggable({
                 setTimeout(function(){click=true},410)
             }
 		}
-*/
+///오리온 소식
+$(".orion_notice .dotarea .orion_notice_dot").click(function(){
+    var i= $(this).index();
+    var notice_width=$(".orion_notice_notice").width()+10
+    $(this).addClass("on")
+    $(this).siblings().removeClass("on")
+    $(".orion_notice_list").css({"left":notice_width*3 * -i + "px"})
+})
 
-
-
+$("#main .orion_event .orion_event_viewport ul li div:first-of-type").click(function(){
+    var li = $(this).parent().parent().parent().children("ul").children("li")
+    var li_1 = $(this).parent().parent().parent().children("ul").children("li:first-of-type")
+    var li_2 = $(this).parent().parent().parent().children("ul").children("li:nth-of-type(2)")
+    var li_3 = $(this).parent().parent().parent().children("ul").children("li:nth-of-type(3)")
+    var ul = $(this).parent().parent().parent().children("ul")
+    $(li_1).show();
+    $(li_3).show();
+    $(ul).css("transition","calc(0.5s /2)");
+    $(li).css("transition","calc(0.5s /2)");
+    $(ul).css("transform","rotateX(0deg) translateY(0px) translateZ(-150px)");
+     setTimeout(function(){
+         $(ul).css("transform","rotateX(90deg) translateY(-450px) translateZ(0px)");
+         $(li_2).css("background","rgba(180,180,180,1.00)");
+         $(li_3).css("background","#ffcece");
+                         },500/2)
+    setTimeout(function(){
+        $(ul).css("transform","rotateX(90deg) translateY(-300px) translateZ(0px)");
+                         },500*2/2)
+    setTimeout(function(){
+        $(ul).css("transition","0s");
+        $(li).css("transition","0s");
+        $(ul).css("transform","rotateX(0deg) translateY(0px) translateZ(0px)");
+        $(li_1).appendTo(ul);
+        $(li_1).hide();
+        $(li_2).hide();
+    },500*3/2)
+})
+$("#main .orion_event .orion_event_viewport ul li div:last-of-type").click(function(){
+    var li = $(this).parent().parent().parent().children("ul").children("li")
+    var li_1 = $(this).parent().parent().parent().children("ul").children("li:first-of-type")
+    var li_2 = $(this).parent().parent().parent().children("ul").children("li:nth-of-type(2)")
+    var li_3 = $(this).parent().parent().parent().children("ul").children("li:nth-of-type(3)")
+    var ul = $(this).parent().parent().parent().children("ul")
+    $(li_1).show();
+    $(li_3).show();
+    $(ul).css("transition","calc(0.5s / 2)");
+    $(li).css("transition","calc(0.5s / 2)");
+    $(ul).css("transform","rotateX(0deg) translateY(0px) translateZ(-150px)");
+     setTimeout(function(){
+         $(ul).css("transform","rotateX(-90deg) translateY(300px) translateZ(300px)");
+         $(li_2).css("background","rgba(180,180,180,1.00)")
+         $(li_1).css("background","#ffcece")
+                         },500/2)
+    setTimeout(function(){
+        $(ul).css("transform","rotateX(-90deg) translateY(0px) translateZ(300px)");
+                         },500*2/2)
+    setTimeout(function(){
+        $(ul).css("transition","0s");
+        $(li).css("transition","0s");
+        $(ul).css("transform","rotateX(0deg) translateY(0px) translateZ(0px)");
+        $(li_3).prependTo(ul)
+        $(li_2).hide();
+        $(li_3).hide();
+                         },500*3/2)
+    
+})
+var daycalc= function(i){
+    let date = $("#main .orion_event .orion_event_viewport ul li:nth-of-type("+i+") a p.fordate").html();
+    let date_arr = date.split(".")
+    let datedata = new Date(date_arr[0],date_arr[1]-1,date_arr[2])
+    let currentdate = new Date()
+    let deffdate = Math.round((datedata-currentdate)/(1000*60*60*24))
+    $("#main .orion_event .orion_event_viewport ul li:nth-of-type("+i+") a p.date").html(deffdate+"일 남음")
+}
+daycalc(1);
+daycalc(2);
+daycalc(3);
